7a176d71181cc8ebf38a61ed4d9563166f9bd952
Youying Lin <liny33@cs.washington.edu>
2015-11-21 09:44:57 -0800

    new node typable

--- app/src/main/java/com/example/liny33/notetaking2/EditorActivity.java
+++ app/src/main/java/com/example/liny33/notetaking2/EditorActivity.java
@@ -31,19 +31,8 @@ public class EditorActivity extends AppCompatActivity {
             setTitle(getString(R.string.new_note));
         }
 
-/*
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);*/
+
 
 
     }
