67614d55e70316fe4fc253fd227f73d2ed4836b1
Youying Lin <liny33@cs.washington.edu>
2015-11-21 00:57:20 -0800

    wth

--- app/src/main/java/com/example/liny33/notetaking2/MainActivity.java
+++ app/src/main/java/com/example/liny33/notetaking2/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.liny33.notetaking2;
 
+import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,7 +20,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        ContentValues values = new ContentValues();
+        values.put(DBOpenHelper.NOTE_TEXT, "New note");
+        Uri noteUri = getContentResolver().insert(NotesProvider.CONTENT_URI,
+                values);
+        Log.d("MainActivity", "Inserted note" + noteUri.getLastPathSegment());
     }
 
     @Override
